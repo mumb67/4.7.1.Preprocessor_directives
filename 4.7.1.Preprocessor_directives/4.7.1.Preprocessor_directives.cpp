f5c7d09fad0b24c2f7f9a5c8304297f36051ce55
Mumb <alik.sanya.07@mail.ru>
2024-06-24 17:50:21 +0300

    Project added

---
+++ 4.7.1.Preprocessor_directives/4.7.1.Preprocessor_directives.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error MODE_NOT_DEFINE.
+#endif
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+#if MODE == 0
+	std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+	std::cout << "Работаю в боевом режиме" << std::endl;
+	int add(int a, int b); {
+		int a, b;
+		std::cout << "Введите два числа" << std::endl;
+		std::cin >> a >> b;
+		a += b;
+		std::cout << "Результат сложения: " << a << std::endl;
+	}
+	add;
+#else
+	std::cout << "Неизвестный режим. Завершение работы." << std::endl;
+	return 0;
+#endif
+	return 0;
+}
+
